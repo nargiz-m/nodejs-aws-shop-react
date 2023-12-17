544cd798766c935ceeeaf7ca0d5f9e5053fac250
Nargiz <55060417+nargiz-m@users.noreply.github.com>
2023-12-17 22:20:05 +0300

    [T-6] updated products link (#5)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://e93bk9ngf2.execute-api.us-east-1.amazonaws.com/prod",
+  product: "https://c9c9kmpx9f.execute-api.us-east-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://3u5qilpipk.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",

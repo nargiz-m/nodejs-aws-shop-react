5895b7605a10b07ffec5baa24637053a80daf6df
Nargiz <55060417+nargiz-m@users.noreply.github.com>
2024-01-07 14:05:17 +0300

    [T-9] cart api link updated (#9)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,11 @@
 const API_PATHS = {
   product: "https://c9c9kmpx9f.execute-api.us-east-1.amazonaws.com/prod",
-  order: "https://y9436f5pg8.execute-api.us-east-1.amazonaws.com/prod",
+  // order: "https://y9436f5pg8.execute-api.us-east-1.amazonaws.com/prod", Cart Lambda Sevice Link
+  order: "https://dd795hy6ag.execute-api.us-east-1.amazonaws.com/",
   import: "https://3u5qilpipk.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://y9436f5pg8.execute-api.us-east-1.amazonaws.com/prod",
+  // cart: "https://y9436f5pg8.execute-api.us-east-1.amazonaws.com/prod", Cart Lambda Sevice Link
+  cart: "https://dd795hy6ag.execute-api.us-east-1.amazonaws.com/",
 };
 
 export default API_PATHS;
